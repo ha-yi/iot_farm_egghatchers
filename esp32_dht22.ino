70ba7eff2e6b7b1f6b62c755124d43e8dff408ac
Hayi Nukman <hayi.nkm@gmail.com>
2019-07-02 20:35:48 +0800

    peot

--- esp32_dht22.ino
+++ esp32_dht22.ino
@@ -3,6 +3,16 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <string.h>
+#include <GxEPD.h>
+#include <GxGDEH029A1/GxGDEH029A1.h>
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <Fonts/FreeSansBold24pt7b.h>
+#include "image.h"
+
+
+#define SCREEN_WIDTH 296 // epaper display width, in pixels
+#define SCREEN_HEIGHT 128 // epaper display height, in pixels
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -11,15 +21,20 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 
-
 DHTesp dht;
 pthread_t threads[5];
-int motorPin = 22;
+int motorPin = 32;
 bool listen = true;
 
 bool stopTemp = false;
 bool stopHumid = false;
 bool stopMotor = false;
+
+float temperature = 0;
+float humid = 0;
+
+GxIO_Class io(SPI, SS, 22, 21);
+GxEPD_Class display(io, 16, 4);
 
 // todo
 /**
@@ -70,32 +85,61 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 
 void *readTemp(void *i) {
   while(listen) {
-    float temperature = dht.getTemperature();
-    Serial.print("[");
-    Serial.print((int)i);
-    Serial.print("] ");
-    
-    Serial.print("Temperature: ");
-    Serial.println(temperature);
-    delay(10000);
+    float t = dht.getTemperature();
+    if (!isnan(t)) temperature = t;
+    delay(1000);
   }
   stopTemp = true;
 }
 
 void *readHumid(void *i) {
   while(listen) {
-    float humid = dht.getHumidity();
-    Serial.print("[");
-    Serial.print((int)i);
-    Serial.print("] ");
-    
-    Serial.print("Humidity ");
-    Serial.println(humid);
-    delay(20000);
+    float h = dht.getHumidity();
+    if(!isnan(h)) humid = h;
+    delay(1000);
   }
   stopHumid = true;
 }
 
+
+void *renderOled(void *i) {
+  int ct = 0;
+  while(listen) {
+    String temperatureString = String(temperature,1);
+    String humidString = String(humid,1);
+    const char* name = "FreeSansBold24pt7b";
+    const GFXfont* f = &FreeSansBold24pt7b;
+  
+    uint16_t box_x = 5;
+    uint16_t box_y = 5;
+    uint16_t box_w = SCREEN_WIDTH-10;
+    uint16_t box_h = SCREEN_HEIGHT-10;
+    uint16_t cursor_y = box_y + 16;
+
+    display.setRotation(45);
+    display.setFont(f);
+    display.setTextColor(GxEPD_BLACK);
+    display.fillScreen(GxEPD_WHITE);
+    display.drawExampleBitmap(bgImg, 5, 5, box_w, box_h, GxEPD_BLACK);
+    if (ct ==0) {
+      display.drawExampleBitmap(telur1, box_w-50, 5, 60, 60, GxEPD_BLACK);
+      ct++;
+    } else if(ct == 1) {
+      display.drawExampleBitmap(telur2, box_w-50, 5, 60, 60, GxEPD_BLACK);
+      ct++;
+    } else {
+      display.drawExampleBitmap(telur3, box_w-50, 5, 60, 60, GxEPD_BLACK);
+      ct = 0;
+    }
+    display.setCursor(box_x+40, box_h -13);
+    display.print(temperatureString); 
+    display.setCursor(box_x+145, box_h -13);
+    display.print(humidString); 
+    display.updateWindow(box_x, box_y, box_w, box_h, true);
+//    display.update();
+    delay(700);
+  }
+}
 bool motorOn = false;
 
 void *triggerMotor(void *i) {
@@ -154,10 +198,16 @@ void startThreadMotor() {
   pthread_create(&threads[i], NULL, triggerMotor, (void *)i);
 }
 
+void startThreadOled() {
+  int i = 3;
+  pthread_create(&threads[i], NULL, renderOled, (void *)i);
+}
+
 void startThreads() {
   startThreadTemp();
   startThreadHumid();
   startThreadMotor();
+  startThreadOled();
 }
 
 void setup()
@@ -165,14 +215,21 @@ void setup()
   Serial.begin(115200);
   pinMode(motorPin, OUTPUT);
   dht.setup(14, DHTesp::DHT22);
+  initBLE();
+
+  display.init();
+  display.fillScreen(GxEPD_WHITE);
+  display.setRotation(45);
+  display.drawExampleBitmap(linov, 5, 5, SCREEN_WIDTH-10, SCREEN_HEIGHT-10, GxEPD_BLACK);
+  display.update();
+  delay(3000);
 
   startThreads();
-  initBLE();
- 
 }
 
 void loop()
 {
+  
   if(stopTemp && listen) {
     stopTemp = false;
     startThreadTemp();
